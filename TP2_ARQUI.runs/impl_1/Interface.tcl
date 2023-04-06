9fe1b478257aaf175f8539f9299dde3765afc955
nehemiasmercauunc <nehemias.mercau@mi.unc.edu.ar>
2023-04-05 21:44:38 -0300

    intento fracasado

--- TP2_ARQUI.runs/impl_1/Interface.tcl
+++ TP2_ARQUI.runs/impl_1/Interface.tcl
@@ -115,7 +115,6 @@ proc step_failed { step } {
 OPTRACE "impl_1" END { }
 }
 
-set_msg_config -id {Common 17-41} -limit 10000000
 
 OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Write Bitstream" START { ROLLUP_AUTO }
@@ -125,6 +124,7 @@ set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
   set_param chipscope.maxJobs 1
+  set_param xicom.use_bs_reader 1
   open_checkpoint Interface_routed.dcp
   set_property webtalk.parent_dir C:/Users/nehemias/Desktop/facu/repo/TP2_ARQUI/TP2_ARQUI.cache/wt [current_project]
 set_property TOP Interface [current_fileset]
